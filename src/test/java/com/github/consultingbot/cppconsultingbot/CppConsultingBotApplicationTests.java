8d3114793d6fe3eda05cebe98b5f34535de7481a
FonyP <misha.ignatenko.2017@bk.ru>
2024-10-31 17:15:42 +0300

    CCB-0: added SpringBoot skeleton to the project

---
+++ src/test/java/com/github/consultingbot/cppconsultingbot/CppConsultingBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.consultingbot.cppconsultingbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CppConsultingBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
